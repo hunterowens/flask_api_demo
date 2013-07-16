e1dc5ef9b33c4a1b0e2a98b2cdeb7db7c792b754
Hunter Owens <owens.hunter@gmail.com>
2013-07-16 13:42:17 -0500

    simple json

--- app.py
+++ app.py
@@ -1,5 +1,10 @@
-from flask import Flask
+from flask import Flask,jsonify
+import json
 app = Flask(__name__)
+
+@app.route('/user')
+def user():
+	return json.dumps({"id":5,"name":"hunter"})
 
 @app.route('/')
 def hello_world():
